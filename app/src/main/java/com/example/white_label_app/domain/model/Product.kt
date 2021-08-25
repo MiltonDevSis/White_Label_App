3825691b5677dbb9cf4e37f57b9bda7630f90cf8
MiltonDevSis <milton.dev.sis@gmail.com>
2021-08-24 21:07:46 -0300

    Implementando FirebaseDataSource metodo get()

--- app/src/main/java/com/example/white_label_app/domain/model/Product.kt
+++ app/src/main/java/com/example/white_label_app/domain/model/Product.kt
@@ -1,8 +1,15 @@
 package com.example.white_label_app.domain.model
 
+import android.os.Parcelable
+import com.google.firebase.firestore.PropertyName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Product(
     val id : String = "",
     val description: String = "",
     val price: Double = 0.0,
+    @get:PropertyName("image_url")
+    @set:PropertyName("iimage_url")
     var imageUrl: String = ""
-)
+) : Parcelable
