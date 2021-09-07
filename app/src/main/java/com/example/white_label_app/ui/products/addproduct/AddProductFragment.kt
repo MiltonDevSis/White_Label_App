5888c93917d1912a5b13b382fac188e61210bdab
MiltonDevSis <milton.dev.sis@gmail.com>
2021-09-06 23:17:01 -0300

    criado formulario e seleção de imagem

--- app/src/main/java/com/example/white_label_app/ui/products/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/example/white_label_app/ui/products/addproduct/AddProductFragment.kt
@@ -1,11 +1,15 @@
 package com.example.white_label_app.ui.products.addproduct
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.white_label_app.databinding.AddProductFragmentBinding
+import com.example.white_label_app.utils.CurrencyTextWatcher
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddProductFragment : BottomSheetDialogFragment() {
@@ -14,6 +18,13 @@ class AddProductFragment : BottomSheetDialogFragment() {
     private val binding get() = _binding!!
     private lateinit var viewModel: AddProductViewModel
 
+    private var imageUri: Uri? = null
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            imageUri = uri
+            binding.imageProduct.setImageURI(uri)
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,7 +32,32 @@ class AddProductFragment : BottomSheetDialogFragment() {
     ): View {
         _binding = AddProductFragmentBinding.inflate(inflater, container, false)
         return _binding!!.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListener()
+    }
+
+    private fun setListener(){
+        binding.imageProduct.setOnClickListener {
+            chooseImage()
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val price = binding.inputPrice.text.toString()
+        }
+
+        binding.inputPrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this))
+        }
+    }
+
+    private fun chooseImage(){
+        getContent.launch("image/*")
+    }
 
 }
